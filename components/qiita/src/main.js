3051fd0cb2fc64cfcba2c66f0b27c2db4f9d4805
kojiro.ueda <mxcpy608@hotmail.com>
2020-11-17 20:15:01 +0900

    Add vue app

---
+++ components/qiita/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+class VueApp extends HTMLElement {
+  constructor() {
+    this.shadowRoot = this.attachShadow;
+  }
+
+  connectedCallback() {
+    createApp(App).mount(this);
+  }
+}
+
+customElements.define("qiita-list");
